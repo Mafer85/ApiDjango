5edf9d2f30a8faa6d43c1a5376f4bc775d2f4c6b
mafer85 <mendozamafer2000@gmail.com>
2020-12-14 10:20:48 -0600

    cambios

--- carp1/urls.py
+++ carp1/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', GraphQLView.as_view(graphiql=True)),
