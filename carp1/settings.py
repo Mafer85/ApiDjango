9368126bad144e8b9be8a02d048982b285a586d3
mafer85 <mendozamafer2000@gmail.com>
2020-12-14 10:17:31 -0600

    cambios

--- carp1/settings.py
+++ carp1/settings.py
@@ -125,10 +125,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 GRAPHENE ={
 'SCHEMA':'carp1.schema.schema'
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
