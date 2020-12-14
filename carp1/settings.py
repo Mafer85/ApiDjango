8433955575d02c8fb90f5c12b68fad139a2497ef
mafer85 <mendozamafer2000@gmail.com>
2020-12-14 14:13:03 -0600

    cambios

--- carp1/settings.py
+++ carp1/settings.py
@@ -35,6 +35,17 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
+CORS_ALLOW_HEADERS=[
+    'accept',
+    'accept-enconding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 CORS_ALLOWED_ORIGINS=['*']
 
