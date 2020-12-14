9e786269b7c0b8e36401e997521d7cbf3f89709e
mafer85 <mendozamafer2000@gmail.com>
2020-12-14 13:28:32 -0600

    cambios

--- carp1/settings.py
+++ carp1/settings.py
@@ -27,7 +27,18 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ALLOWED_ORIGINS=[
+    '*'
+]
 
+CORS_ALLOWED_METHODS=[
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'corsheaders',
     'carp2'
 ]
 
@@ -50,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'carp1.urls'
